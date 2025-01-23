9e6bb357689e7d5582f55e1bc9a888c383c8635d
Ashish Malgatti <bridgeandseekdev@gmail.com>
2025-01-23 13:05:41 +0530

    Removing custom auth

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,15 +1,12 @@
 // pages/_app.tsx
 import { AppProps } from 'next/app';
 import { ThemeProvider } from '../context/ThemeContext';
-import { AuthProvider } from '../context/AuthContext';
 import '../styles/globals.css';
 
 function MyApp({ Component, pageProps }: AppProps) {
   return (
     <ThemeProvider>
-      <AuthProvider>
-        <Component {...pageProps} />
-      </AuthProvider>
+      <Component {...pageProps} />
     </ThemeProvider>
   );
 }
